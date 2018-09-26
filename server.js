6cdddd07ea9e54a068c5f4f7bf061f5d1ccee55b
Alper Donmez <alper.donmez490@gmail.com>
2018-09-26 13:51:57 -0400

    setup start script and heroku port

--- server.js
+++ server.js
@@ -37,6 +37,8 @@ app.get('/about', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
     console.log('Server is up on port 3000');
 });
